8ad94cb9aa4f8eea6da68a217395cb52ea74943b
karcerr <roma.mershon@mail.ru>
2025-01-05 18:01:32 +0500

    Added fragment animations.

--- app/src/main/java/com/stepikbrowser/MainActivity.kt
+++ app/src/main/java/com/stepikbrowser/MainActivity.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stepikbrowser.databinding.MainActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +21,27 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val currentDestination = navController.currentDestination?.id
+            if (currentDestination == item.itemId) { // Preventing navigating to the fragment we are currently in
+                return@setOnItemSelectedListener false
+            }
+            val navOptions = when (item.itemId) {
+                R.id.homeFragment -> createNavOptions(currentDestination, R.id.homeFragment)
+                R.id.favoritesFragment -> createNavOptions(currentDestination, R.id.favoritesFragment)
+                R.id.profileFragment -> createNavOptions(currentDestination, R.id.profileFragment)
+                else -> null
+            }
+
+            if (navOptions != null) {
+                navController.navigate(item.itemId, null, navOptions)
+                true
+            } else {
+                false
+            }
+        }
+        bottomNavigationView.menu.findItem(R.id.homeFragment).setChecked(true)
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             //Clearing these because we never back down
             navController.popBackStack(R.id.splashFragment, true)
@@ -38,5 +57,33 @@ class MainActivity : AppCompatActivity() {
                 else -> bottomNavigationView.visibility = View.GONE
             }
         }
+    }
+    private fun createNavOptions(currentDestination: Int?, targetDestination: Int): NavOptions {
+        return NavOptions.Builder()
+            .setEnterAnim(
+                if (currentDestination != null && currentDestination < targetDestination)
+                    R.anim.slide_in_right
+                else
+                    R.anim.slide_in_left
+            )
+            .setExitAnim(
+                if (currentDestination != null && currentDestination < targetDestination)
+                    R.anim.slide_out_left
+                else
+                    R.anim.slide_out_right
+            )
+            .setPopEnterAnim(
+                if (currentDestination != null && currentDestination > targetDestination)
+                    R.anim.slide_in_left
+                else
+                    R.anim.slide_in_right
+            )
+            .setPopExitAnim(
+                if (currentDestination != null && currentDestination > targetDestination)
+                    R.anim.slide_out_right
+                else
+                    R.anim.slide_out_left
+            )
+            .build()
     }
 }
