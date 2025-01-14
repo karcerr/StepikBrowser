dbec651b50a5a302bcd146a1998404395fc3e769
karcerr <roma.mershon@mail.ru>
2025-01-14 20:49:00 +0500

    Started working on Profile Fragment

--- feature/profile/src/main/java/com/stepikbrowser/feature/profile/ProfileFragment.kt
+++ feature/profile/src/main/java/com/stepikbrowser/feature/profile/ProfileFragment.kt
@@ -1,8 +1,18 @@
 package com.stepikbrowser.feature.profile
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.stepikbrowser.feature.profile.databinding.ProfileFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProfileFragment: Fragment() {
+class ProfileFragment: Fragment(R.layout.profile_fragment) {
+    private lateinit var binding: ProfileFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = ProfileFragmentBinding.bind(view)
+
+    }
 }
